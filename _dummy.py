631e7198110bd32c076f3d2efe638d06c04921c4
Foo <F.C.Gootjes@student.rug.nl>
2016-06-26 19:24:59 +0200

    memory IO for zipfile

--- _dummy.py
+++ _dummy.py
@@ -7,7 +7,7 @@ extensions.load_extension("vlc")
 import plugins
 plugins.load_plugin("jazzradio_com")
 plugins.get_plugin("jazzradio_com").init()
-channels=plugins.get_plugin("jazzradio_com").plugin.load_channels()
-plugins.get_plugin("jazzradio_com").plugin.initialize_dict()
-print(plugins.get_plugin("jazzradio_com").plugin.get_currently_playing_channel('mellowpianojazz'))
-j=plugins.get_plugin("jazzradio_com").plugin
+#channels=plugins.get_plugin("jazzradio_com").plugin.load_channels()
+#plugins.get_plugin("jazzradio_com").plugin.initialize_dict()
+#print(plugins.get_plugin("jazzradio_com").plugin.get_currently_playing_channel('mellowpianojazz'))
+#j=plugins.get_plugin("jazzradio_com").plugin
--- plugins/jazzradio_com/__init__.py
+++ plugins/jazzradio_com/__init__.py
@@ -5,12 +5,34 @@ NAMB_VERSION="0.1"
 MODULE=None
 
 import importlib
+import zipfile
+import urllib2
+import os
+import StringIO
 
 def is_prepared():
-    return True
+    return os.path.isdir(__path__[0]+os.sep+"jazzradio_com")
 
+def remove():
+    os.remove(__path__[0]+os.sep+"jazzradio_com")
+
+#UNTESTED
 def prepare():
-    pass
+    response = urllib2.urlopen("https://github.com/NAMB-develop/jazzradio_com/archive/master.zip")
+    zipcontent = response.read()
+    s=StringIO.StringIO()
+    s.write(zipcontent)
+    z=zipfile.ZipFile(s,'r')
+
+    ss=StringIO.StringIO()
+    zz=zipfile.ZipFile(ss,'w')
+    for member in z.namelist():
+        if member.split('jazzradio_com-master/')[1]:
+            zz.writestr('jazzradio_com/'+member.split('jazzradio_com-master/')[1])
+
+    zz.extractall(__path__[0])
+    zz.close()
+    z.close()
 
 def load():
     MODULE=importlib.import_module("plugins.jazzradio_com.jazzradio_com")
